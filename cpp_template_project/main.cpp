24ecba43ff644d9613a3832da90ddeb408e21f1f
Ricardo Bueno <ricardo.bueno.sf.dev@hotmail.com>
2023-01-03 23:27:45 -0300

    Template project

---
+++ cpp_template_project/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+consteval int get_value() {
+    return 3;
+}
+
+int main() {
+    constexpr int value = get_value();
+    std::cout << "value : " << value << '\n';
+    return 0;
+}
